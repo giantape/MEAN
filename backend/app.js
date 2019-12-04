44117a2be00dc7387f39fb7ff093160903790fbe
sapinodejs@gmail.com <>
2019-12-04 14:20:46 +0100

    add mongoDb. closes #3

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const PostSchema = require("./models/postsModel");
+const dbConfig = require('./config/config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
 
 const app = express();
 
@@ -20,30 +34,49 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new PostSchema({
+      title: req.body.title,
+      content: req.body.content
+  })
   console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
-  });
+  post.save().then(result => {
+    res.status(201).json({
+        message: 'Post added successfully',
+        postId: result._id
+      });
+  })
+
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
+    PostSchema.find()
+    .then(posts => {
+        res.send(posts);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Error No Record in the Database."
+        });
+    });
 });
 
+app.delete("/api/posts/:id", (req, res) => {
+    PostSchema.findByIdAndRemove(req.params.id)
+        .then(post => {
+            if (!post) {
+                return res.status(404).send({
+                    message: "user not found with id " + req.params.id
+                });
+            }
+            res.send({ message: "user deleted successfully!" });
+        }).catch(err => {
+            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+                return res.status(404).send({
+                    message: "user not found with id " + req.params.id
+                });
+            }
+            return res.status(500).send({
+                message: "Could not delete user with id " + req.params.id
+            });
+        });
+})
 module.exports = app;
